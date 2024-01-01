f4878044b5a88f97a57d691f5c6fc42409dc5ef3
michaelkalai <michael.kalai124@gmail.com>
2023-12-31 19:07:39 -0500

    minor adjustments to work with men's division

--- division.py
+++ division.py
@@ -2,9 +2,10 @@ from gymnast import Gymnast
 
 # class for storing (and eventually analyzing) each gymnast in either the men's or women's division
 class Division:
-    def __init__(self, gender):
+    def __init__(self, gender, srange):
         self.gender = gender
         self.gymnasts = {}
+        self.srange = srange
     
     def addperformance(self, name, gender, country, date, competition, round, location, apparatus, rank, dscore, escore, penalty, score):
         if name in self.gymnasts:
@@ -29,7 +30,7 @@ class Division:
         # gets a list of all american gymnasts within a given range of the max score
         medalopportunity = []
         for gymnast in self.gymnasts:
-            couldplace, score = self.gymnasts[gymnast].medalopportunity(apparatus, maxscore)
+            couldplace, score = self.gymnasts[gymnast].medalopportunity(apparatus, maxscore, self.srange)
             if couldplace:
                 medalopportunity.append(tuple([gymnast, score]))
         medalopportunitysorted = []
@@ -55,7 +56,6 @@ class Division:
         count = 0
         apparatus_performances = []
         unique_gymnast_names = set()
-        apparatus_names = []
         # Find the score of the specified gymnast
         for i in self.gymnasts.values():
             if i.name == name:
@@ -107,7 +107,7 @@ class Division:
                 peoplewithchance.add(people[0])
 
 
-        while(j<4):
+        while(j<len(apparatuses)):
 
             
             for people in peoplewithchance:
